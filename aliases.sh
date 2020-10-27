cae6ba8281863dc1f41012b2d55c2f0d7a6af711
Eugene Sinamban <sinamban_e@cyberowl.co.jp>
2020-10-27 23:53:26 +0900

    set up dev

--- aliases.sh
+++ aliases.sh
@@ -1,8 +1,9 @@
 # lcl
 
-alias lcl-build="docker-compose build php-fpm lcl"
-alias lcl="lcl-build && docker-compose up lcl"
-alias lcl-d="lcl-build && docker-compose up -d lcl"
-alias lcl-logs="docker-compose logs -f --tail 100 lcl"
-alias lcl-restart="docker-compose restart lcl"
-alias lcl-bash="docker-compose exec lcl bash"
+alias admin-lcl-build="docker-compose build php-fpm lcl"
+alias admin-lcl="admin-lcl-build && docker-compose up lcl"
+alias admin-lcl-d="admin-lcl-build && docker-compose up -d lcl"
+alias admin-lcl-logs="docker-compose logs -f --tail 100 lcl"
+alias admin-lcl-restart="docker-compose restart lcl"
+alias admin-lcl-db-bash="docker-compose exec db bash"
+alias admin-lcl-bash="docker-compose exec lcl bash"
